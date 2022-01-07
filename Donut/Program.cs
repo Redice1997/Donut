c4f5f143695e5872a662c21fbd4a794260ef0c51
Redice1997 <roshin-vladimir@list.ru>
2022-01-07 19:35:34 +0300

    .

--- Donut/Program.cs
+++ Donut/Program.cs
@@ -7,25 +7,7 @@ namespace Donut
     {
         static void Main(string[] args)
         {
-            var recievedBytes = new byte[256];
-
-            for (int i = 0; i < recievedBytes.Length; i++)
-            {
-                recievedBytes[i] = (byte)i;
-            }
-
-            var str = Encoding.GetEncoding(866).GetString(recievedBytes);
-
-            for (int i = 0; i < str.Length; i++)
-            {
-                Console.Write(str[i] + " ");
-                if (i % 16 == 0)
-                {
-                    Console.WriteLine();
-                }
-            }
-
-            Console.ReadKey();
+            
         }
     }
 }
--- Donut/Vector2.cs
+++ Donut/Vector2.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Donut
 {
-    class Class1
+    struct Vector2
     {
+        public float x { get; private set; }
+        public float y { get; private set; }
+
+        Vector2(float value)
+        {
+            x = value;
+            y = value;
+            
+        }
+       
+        Vector2(float x, float y) 
+        {
+            this.x = x;
+            this.y = y;
+            
+        }
+        public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.x + v2.x, v1.y + v2.y);
+        public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.x - v2.x, v1.y - v2.y);
     }
 }
