0e6a7cc7e20347781232f0537e8dbe4f48280284
Redice1997 <roshin-vladimir@list.ru>
2022-01-09 00:45:48 +0300

    .

--- Donut/Vector2.cs
+++ Donut/Vector2.cs
@@ -8,30 +8,29 @@ namespace Donut
 {
     struct Vector2
     {
-        public float x { get; private set; }
-        public float y { get; private set; }
+        public double x { get; private set; }
+        public double y { get; private set; }
 
-        public Vector2(float value)
+        public Vector2(double value)
         {
             x = value;
             y = value;            
         }
        
-        public Vector2(float x, float y) 
+        public Vector2(double x, double y) 
         {
             this.x = x;
             this.y = y;            
         }
         public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.x + v2.x, v1.y + v2.y);
         public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.x - v2.x, v1.y - v2.y);
+        public static Vector2 operator *(Vector2 v, double value) => new Vector2(v.x * value, v.y * value);
+        public static Vector2 operator /(Vector2 v, double value) => new Vector2(v.x / value, v.y / value);
 
-        public static bool operator >(Vector2 v1, float value) => v1.Modul() > value ? true : false;
-        public static bool operator <(Vector2 v1, float value) => v1.Modul() < value ? true : false;
-
-        public void Rotate(float angle)
+        public void Rotate(double angle)
         {
 
         }
-        public float Modul() => MathF.Sqrt(x * x + y * y);        
+        public double Modul() => Math.Sqrt(x * x + y * y);        
     }
 }
--- Donut/Vector3.cs
+++ Donut/Vector3.cs
@@ -8,23 +8,23 @@ namespace Donut
 {
     struct Vector3
     {
-        public float x { get; private set; }
-        public float y { get; private set; }
-        public float z { get; private set; }
+        public double x { get; private set; }
+        public double y { get; private set; }
+        public double z { get; private set; }
 
-        public Vector3(float value)
+        public Vector3(double value)
         {
             x = value;
             y = value;
             z = value;
         }
-        public Vector3(Vector2 v, float z)
+        public Vector3(Vector2 v, double z)
         {
             x = v.x;
             y = v.y;
             this.z = z;
         }
-        public Vector3(float x, float y, float z)
+        public Vector3(double x, double y, double z)
         {
             this.x = x;
             this.y = y;
@@ -33,5 +33,7 @@ namespace Donut
 
         public static Vector3 operator +(Vector3 v1, Vector3 v2) => new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         public static Vector3 operator -(Vector3 v1, Vector3 v2) => new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        public static Vector3 operator *(Vector3 v, double value) => new Vector3(v.x * value, v.y * value, v.z * value);
+        public static Vector3 operator /(Vector3 v, double value) => new Vector3(v.x / value, v.y / value, v.z / value);
     }
 }
